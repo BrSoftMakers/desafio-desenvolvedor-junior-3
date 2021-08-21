e145e3246d419f21ea6b65351a9c8b93f826aa83
wemersonsantos <wemersonsantoseng@gmail.com>
2021-08-21 00:06:26 -0300

    realizando o front end

--- routes/index.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.use('/', require('./register'));
-router.use('/', require('./login'));
-router.use('/', require('./posts'));
-
-module.exports = router
