3d08259b4e31038d55c003cc53e26ed421310eb0
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-17 18:40:40 -0300

    build: configura dados de conexão com o banco

---
+++ app/backend/src/database/config/config.js
@@ -0,0 +1,13 @@
+require('dotenv/config');
+
+Object.defineProperty(exports, '__esModule', { value: true });
+
+const config = {
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || '123456',
+  database: process.env.DB_NAME || 'MyBlog',
+  host: process.env.DB_HOST || 'localhost',
+  port: Number(process.env.DB_PORT || 3002),
+  dialect: 'mysql',
+};
+module.exports = config;
