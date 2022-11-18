93f2099cd966135e6f456dbc827e3401a6b2a4f2
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-17 23:11:29 -0300

    test: implementa teste da funçã insert do service Post
    --Quando implementada, essa função deve inserir um novo Post no banco de dados

--- app/backend/src/tests/unit/Post.service.test.js
+++ app/backend/src/tests/unit/Post.service.test.js
@@ -4,4 +4,28 @@ const { Post } = require('../../database/models');
 const PostService = require('../../services/Post');
 
 describe('Testes unitários do service Post', () => {
+  describe('Testa a função insert ao incluir um post no banco de dados', () => {
+    const postToInsert = {};
+    const insertedPost = {};
+
+    beforeEach(async () => {
+      sinon.stub(Post, 'create').resolves(insertedPost);
+    });
+
+    afterEach(async () => {
+      Post.create.restore();
+    });
+
+    it('Testa se um objeto é retornado', async () => {
+      const response = await PostService.insert(postToInsert);
+
+      expect(response).to.be.an('object');
+    });
+
+    it('Testa se o objeto contém informações da pessoa usuária inserida', async () => {
+      const response = await PostService.insert(postToInsert);
+
+      expect(response).to.be.deep.equal(insertedPost);
+    });
+  });
 });
