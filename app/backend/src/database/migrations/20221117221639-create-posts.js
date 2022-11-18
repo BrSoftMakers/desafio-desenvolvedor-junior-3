845e0de481723ad27a2eba2c149ad602c4951bf6
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 20:51:11 -0300

    fix: muda tipo para que campo content possa receber textos maiores

--- app/backend/src/database/migrations/20221117221639-create-posts.js
+++ app/backend/src/database/migrations/20221117221639-create-posts.js
@@ -15,7 +15,7 @@ module.exports = {
       },
       content: {
         allowNull: false,
-        type: Sequelize.STRING,
+        type: Sequelize.TEXT,
       },
       userId: {
         type: Sequelize.INTEGER,
