e3d9ddac89e18d79f30ee126a9d4dc7bbfb1314e
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-17 21:55:41 -0300

    test: cria arquivo User.service.test para testes unitários do service User

---
+++ app/backend/src/tests/unit/User.service.test.js
@@ -0,0 +1,7 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { User } = require('../../database/models');
+const UserService = require('../../services/User');
+
+describe('Testes unitários do service User', () => {
+});
