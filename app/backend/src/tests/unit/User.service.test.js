88ba951f84d94af392df728743842124f6328042
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 13:06:24 -0300

    test: refatora testes para incluir mocks

--- app/backend/src/tests/unit/User.service.test.js
+++ app/backend/src/tests/unit/User.service.test.js
@@ -1,15 +1,22 @@
 const sinon = require('sinon');
 const { expect } = require('chai');
 const { User } = require('../../database/models');
-const UserService = require('../../services/User');
+const UserService = require('../../services/User.service');
+const {
+  userToInsert,
+  user,
+  recoverdUser,
+  token,
+} = require('./mocks/User.mock');
+const jwt = require('jsonwebtoken');
+require('dotenv/config');
+
+const secret = process.env.JWT_SECRET || jwt_secret;
 
 describe('Testes unitários do service User', () => {
   describe('Testa o comportamento da função insert ao registrar uma pessoa usuária', () => {
-    const userToInsert = {};
-    const insertedUser = {};
-
     beforeEach(async () => {
-      sinon.stub(User, 'create').resolves(insertedUser);
+      sinon.stub(User, 'create').resolves(user);
     });
 
     afterEach(async () => {
@@ -25,17 +32,14 @@ describe('Testes unitários do service User', () => {
     it('Testa se o objeto contém informações da pessoa usuária inserida', async () => {
       const response = await UserService.insert(userToInsert);
 
-      expect(response).to.be.deep.equal(insertedUser);
+      expect(response).to.be.deep.equal(user);
     });
   });
 
   describe('Testa o comportamento da função findByEmail', () => {
     describe('Testa findByEmail se o email passado for de uma pessoa usuária cadastrada no banco', async () => {
-      const email = 'email.cadastrado@email.com';
-      const user = [{}];
-
       beforeEach(async () => {
-        sinon.stub(User, 'findAll').resolves(user);
+        sinon.stub(User, 'findAll').resolves(recoverdUser);
       });
 
       afterEach(async () => {
@@ -43,24 +47,25 @@ describe('Testes unitários do service User', () => {
       });
 
       it('Testa se o retorno é um objeto', async () => {
-        const response = await UserService.findByEmail(email);
+        const response = await UserService.findByEmail({
+          email: 'airel.ribeiro@gmail.com',
+        });
 
         expect(response).to.be.an('object');
       });
 
       it('Testa se o objeto retornado contém informações da pessoa usuária', async () => {
-        const response = await productsServices.getById(1);
+        const response = await UserService.findByEmail({
+          email: 'airel.ribeiro@gmail.com',
+        });
 
-        expect(response).to.be.deep.equal(user[0]);
+        expect(response).to.be.deep.equal(user);
       });
     });
 
     describe('Testa findByEmail se o email passado for de uma pessoa usuária NÃO cadastrada no banco', async () => {
-      const email = 'email.invalido@email.com';
-      const user = [];
-
       beforeEach(async () => {
-        sinon.stub(User, 'findAll').resolves(user);
+        sinon.stub(User, 'findAll').resolves([]);
       });
 
       afterEach(async () => {
@@ -68,12 +73,16 @@ describe('Testes unitários do service User', () => {
       });
 
       it('Testa se é um objeto', async () => {
-        const response = await UserService.findByEmail(email);
+        const response = await UserService.findByEmail({
+          email: 'email.invalido@email.com',
+        });
 
         expect(response).to.be.an('object');
       });
       it('Testa se esse objeto possui a mensagem correta', async () => {
-        const response = await UserService.findByEmail(email);
+        const response = await UserService.findByEmail({
+          email: 'email.invalido@email.com',
+        });
 
         expect(response).to.be.deep.equal({ message: 'userNotFound' });
       });
