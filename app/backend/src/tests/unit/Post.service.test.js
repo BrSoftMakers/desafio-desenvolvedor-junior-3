b433c5f04e0e1c2e1f9e4e2f1434127fee49824a
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-17 23:15:16 -0300

    test: implementa teste da funçã findByUser do service Post
    --Quando houver posts com o id do usuário buscado, a função deve retorná-los;
    Quando não houver, a função deve retornar uma mensagem

--- app/backend/src/tests/unit/Post.service.test.js
+++ app/backend/src/tests/unit/Post.service.test.js
@@ -59,4 +59,57 @@ describe('Testes unitários do service Post', () => {
     });
   });
 
+  describe('Testa a função findByUser que recupera todos os posts de uma pessoa usuária específica no banco de dados', () => {
+    describe('Testa a função findByUser quando há posts daquela pessoa usuária no banco', () => {
+      const postsByUser = [];
+      const userId = 3;
+
+      beforeEach(async () => {
+        sinon.stub(Post, 'findAll').resolves(postsByUser);
+      });
+
+      afterEach(async () => {
+        Post.findAll.restore();
+      });
+
+      it('Testa se o retorno é um array com objetos', async () => {
+        const response = await PostService.findByUser(userId);
+
+        expect(response).to.be.an('array');
+      });
+
+      it('Testa se todos os posts são retornados', async () => {
+        const response = await PostService.findByUser(userId);
+
+        expect(response).to.be.eql([]);
+      });
+    });
+
+    describe('Testa a função findByUser quando não há posts daquela pessoa usuária no banco', () => {
+      const postsByUser = [];
+      const userId = 11;
+
+      beforeEach(async () => {
+        sinon.stub(Post, 'findAll').resolves(postsByUser);
+      });
+
+      afterEach(async () => {
+        Post.findAll.restore();
+      });
+
+      it('Testa se é um objeto', async () => {
+        const response = await PostService.findByUser(userId);
+
+        expect(response).to.be.an('object');
+      });
+
+      it('Testa se esse objeto possui a mensagem correta', async () => {
+        const response = await PostService.findByUser(userId);
+
+        expect(response).to.be.deep.equal({
+          message: 'thereAreNoPostsByThisUser',
+        });
+      });
+    });
+  });
 });
