d59fb36179d6cf9ff6d64f55335c6e116b6d2315
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-17 21:56:54 -0300

    test: implementa testes da função insert
    -- Função responsável por chamar model User e inserir pessoa usuária no banco de dados

--- app/backend/src/tests/unit/User.service.test.js
+++ app/backend/src/tests/unit/User.service.test.js
@@ -4,4 +4,28 @@ const { User } = require('../../database/models');
 const UserService = require('../../services/User');
 
 describe('Testes unitários do service User', () => {
+  describe('Testa o comportamento da função insert ao registrar uma pessoa usuária', () => {
+    const userToInsert = {};
+    const insertedUser = {};
+
+    beforeEach(async () => {
+      sinon.stub(User, 'create').resolves(insertedUser);
+    });
+
+    afterEach(async () => {
+      User.create.restore();
+    });
+
+    it('Testa se um objeto é retornado', async () => {
+      const response = await UserService.insert(userToInsert);
+
+      expect(response).to.be.an('object');
+    });
+
+    it('Testa se o objeto contém informações da pessoa usuária inserida', async () => {
+      const response = await UserService.insert(userToInsert);
+
+      expect(response).to.be.deep.equal(insertedUser);
+    });
+  });
 });
