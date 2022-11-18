b399d2dfe7d88e4fb2ab0330b7550c056f6f555a
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-17 21:59:20 -0300

    test: implementa testes da função findByEmail
    -- Função responsável por chamar model User e buscar pessoa usuária no banco de dados;
    Será retornando a pessoa usuária, se hover;
    Ou um objeto informando que não há cadastro com esse email.

--- app/backend/src/tests/unit/User.service.test.js
+++ app/backend/src/tests/unit/User.service.test.js
@@ -28,4 +28,59 @@ describe('Testes unitários do service User', () => {
       expect(response).to.be.deep.equal(insertedUser);
     });
   });
+
+  describe('Testa o comportamento da função findByEmail', () => {
+    describe('Testa findByEmail se o email passado for de uma pessoa usuária cadastrada no banco', async () => {
+      const email = 'email.cadastrado@email.com';
+      const user = [{}];
+
+      beforeEach(async () => {
+        sinon.stub(User, 'findAll').resolves(user);
+      });
+
+      afterEach(async () => {
+        User.findAll.restore();
+      });
+
+      it('Testa se o retorno é um objeto', async () => {
+        const response = await UserService.findByEmail(email);
+
+        expect(response).to.be.an('object');
+      });
+
+      it('Testa se o objeto retornado contém informações da pessoa usuária', async () => {
+        const response = await productsServices.getById(1);
+
+        expect(response).to.be.deep.equal(user[0]);
+      });
+    });
+
+    describe('Testa findByEmail se o email passado for de uma pessoa usuária NÃO cadastrada no banco', async () => {
+      const email = 'email.invalido@email.com';
+      const user = [];
+
+      beforeEach(async () => {
+        sinon.stub(User, 'findAll').resolves(user);
+      });
+
+      afterEach(async () => {
+        User.findAll.restore();
+      });
+
+      it('Testa se é um objeto', async () => {
+        const response = await UserService.findByEmail(email);
+
+        expect(response).to.be.an('object');
+      });
+      it('Testa se esse objeto possui a mensagem correta', async () => {
+        const response = await UserService.findByEmail(email);
+
+        expect(response).to.be.deep.equal({
+          message:
+            'Email não corresponde a nenhuma pessoa cadastrada no banco de dados!',
+          code: 404,
+        });
+      });
+    });
+  });
 });
