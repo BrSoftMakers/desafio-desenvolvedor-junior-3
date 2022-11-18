06d08c2a5516049c36712e79b5c0ccc63d6687e8
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-17 23:09:44 -0300

    test: cria arquivo de teste unitário para service Post

---
+++ app/backend/src/tests/unit/Post.service.test.js
@@ -0,0 +1,7 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { Post } = require('../../database/models');
+const PostService = require('../../services/Post');
+
+describe('Testes unitários do service Post', () => {
+});
