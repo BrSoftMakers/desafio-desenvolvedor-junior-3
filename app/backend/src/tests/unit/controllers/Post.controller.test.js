d0f3d1455ef03587c8dfaf7ecf192bf18ba56917
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 22:57:22 -0300

    test: implementa teste da função findAll
    --Função chama service para buscar todos o posts no banco de dados

--- app/backend/src/tests/unit/controllers/Post.controller.test.js
+++ app/backend/src/tests/unit/controllers/Post.controller.test.js
@@ -43,6 +43,42 @@ describe('Testes unitários do controller Post', () => {
     });
   });
 
+  describe('Testa o comportamento da função findAll ao recuperar todos os posts do banco de dados', () => {
+    const allPosts = [{}];
+
+    beforeEach(async () => {
+      sinon.stub(PostService, 'findAll').resolves(allPosts);
+    });
+
+    afterEach(async () => {
+      PostService.findAll.restore();
+    });
+
+    it('Testa se o status de retorno é 200', async () => {
+      const req = {};
+      const res = {};
+      req.body = postToInsert;
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      await PostController.findAll(req, res);
+
+      expect(res.status.calledWith(200)).to.be.true;
+    });
+
+    it('Testa se o json é chamado com o post inserido', async () => {
+      const req = {};
+      const res = {};
+
+      res.status = sinon.stub().returns(res);
+      res.json = sinon.stub().returns();
+
+      PostController.findAll(req, res);
+
+      expect(res.json.calledWith(allPosts)).to.be.true;
+    });
+  });
 
   describe('Testa o comportamento da função findByUser', () => {
     describe('Testa findByUser quando há posts da pessoa usuária no banco de dados', () => {
