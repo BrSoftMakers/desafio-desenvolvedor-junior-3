bc3883f27c560e5216991b180676d6b6fd35b5e8
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 22:51:43 -0300

    test: cria arquivo para testes de PostController

---
+++ app/backend/src/tests/unit/controllers/Post.controller.test.js
@@ -0,0 +1,7 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const PostService = require('../../../services/Post.service');
+const PostController = require('../../../controllers/Post.controller');
+
+describe('Testes unitários do controller Post', () => {
+});
