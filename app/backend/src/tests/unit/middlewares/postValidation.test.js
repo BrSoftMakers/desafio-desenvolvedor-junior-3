6cd53c8ca3293ef6b27608f36d20b810c9c9de74
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 23:01:33 -0300

    test: implementa teste para middleware de validação findPost
    -- Middleware para rotas put e delete de posts, verificando se o post em questão existe

---
+++ app/backend/src/tests/unit/middlewares/postValidation.test.js
@@ -0,0 +1,28 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const { findPost } = require('../../../middlewares/postValidation');
+
+describe('Testes unitários do middleware findPost', () => {
+  describe('Testa findPost quando NÃO há um post com o id buscado no banco de dados', () => {
+    beforeEach(async () => {
+      sinon.stub(PostService, 'findById').resolves({ message: 'postNotFound' });
+    });
+
+    afterEach(async () => {
+      PostService.findById.restore();
+    });
+
+    it('Testa se é lançado um erro com a mensagem correta', async () => {
+      const req = {};
+      const res = {};
+      let error;
+      try {
+        req.params = { id: 70 };
+        await findPost(req, res);
+      } catch (err) {
+        error = err;
+      }
+      expect(error.message).to.be.deep.equal('postNotFound');
+    });
+  });
+});
