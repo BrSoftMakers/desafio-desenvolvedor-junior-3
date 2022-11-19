84da568977e982f02f5f1aca64ce674cb9d20c50
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 22:47:46 -0300

    test: cria arquivo de teste para UserController

---
+++ app/backend/src/tests/unit/controllers/User.controller.test.js
@@ -0,0 +1,7 @@
+const sinon = require('sinon');
+const { expect } = require('chai');
+const UserService = require('../../../services/User.service');
+const UserController = require('../../../controllers/User.controller');
+
+describe('Testes unitários do controller User', () => {
+});
