ac63e2a37afc22f37b00e1e08359e54978e06b7f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-19 13:03:16 -0300

    feat: implementa função de login

--- app/backend/src/controllers/User.controller.js
+++ app/backend/src/controllers/User.controller.js
@@ -1,6 +1,13 @@
 const UserService = require('../services/User.service');
 
 const UserController = {
+  login: async (req, res) => {
+    const user = await UserService.findUser(req.body);
+    if (user.message) throw new Error(user.message);
+    const token = UserService.generateToken(user);
+    return res.status(200).json({ token });
+  },
+
 };
 
 module.exports = UserController;
