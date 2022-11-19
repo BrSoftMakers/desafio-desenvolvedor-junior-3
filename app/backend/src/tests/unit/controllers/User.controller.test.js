74a2faed154223ae4429d0125a643cdd8ab8a01e
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 22:49:43 -0300

    test: implementa testes da função de login
    -- Se houver pessoa usuária, deve ser retornado um token;
    -- Se não houver deve ser lançado um erro

--- app/backend/src/tests/unit/controllers/User.controller.test.js
+++ app/backend/src/tests/unit/controllers/User.controller.test.js
@@ -4,4 +4,73 @@ const UserService = require('../../../services/User.service');
 const UserController = require('../../../controllers/User.controller');
 
 describe('Testes unitários do controller User', () => {
+  describe('Testa o comportamento da função login', () => {
+    describe('Testa login quando a pessoa usuária existe no banco de dados', () => {
+      const user = {};
+
+      beforeEach(async () => {
+        sinon.stub(UserService, 'findByEmail').resolves(user);
+      });
+
+      afterEach(async () => {
+        UserService.findByEmail.restore();
+      });
+
+      it('Testa se o status de retorno é 200', async () => {
+        const req = {};
+        const res = {};
+        req.body = { email: 'airel.ribeiro@gmail.com', password: '123456789' };
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        await UserController.login(req, res);
+
+        expect(res.status.calledWith(200)).to.be.true;
+      });
+
+      it('Testa se o json é chamado com um objeto contendo um token', async () => {
+        const req = {};
+        const res = {};
+        req.body = { email: 'airel.ribeiro@gmail.com', password: '123456789' };
+        const json = { token: '' };
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        await UserController.login(req, res);
+
+        expect(res.json.calledWith(json)).to.be.true;
+      });
+    });
+
+    describe('Testa login quando a pessoa usuária NÃO existe no banco de dados', () => {
+      beforeEach(async () => {
+        sinon
+          .stub(UserService, 'findByEmail')
+          .resolves({ message: 'userNotFound' });
+      });
+
+      afterEach(async () => {
+        UserService.findByEmail.restore();
+      });
+
+      it('Testa se é lançado um erro com a mensagem correta', async () => {
+        const req = {};
+        const res = {};
+        let error;
+        try {
+          req.body = {
+            email: 'invalid.email@email.com',
+            password: '999999999',
+          };
+          await UserController.login(req, res);
+        } catch (err) {
+          error = err;
+        }
+        expect(error.message).to.be.deep.equal('userNotFound');
+      });
+    });
+  });
+
 });
