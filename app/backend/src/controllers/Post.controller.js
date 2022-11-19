485710a46c9da3e67c89bc2375ea81cf445a7f6a
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-19 13:56:02 -0300

    feat: implementa insert
    -- Função para inserir novos posts no banco de dados

--- app/backend/src/controllers/Post.controller.js
+++ app/backend/src/controllers/Post.controller.js
@@ -1,6 +1,11 @@
 const PostService = require('../services/Post.service');
 
 const PostController = {
+  insert: async (req, res) => {
+    const post = await PostService.insert(req.body);
+    return res.status(201).json(post);
+  },
+
 };
 
 module.exports = PostController;
