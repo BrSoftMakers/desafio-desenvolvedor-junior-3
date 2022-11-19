4e917249a5e185cc7566ab3bd5df24d276d5557d
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 22:54:17 -0300

    test: implementa teste da função findAll
    --Função chama service para buscar todos o posts no banco de dados

--- app/backend/src/tests/unit/controllers/Post.controller.test.js
+++ app/backend/src/tests/unit/controllers/Post.controller.test.js
@@ -4,19 +4,19 @@ const PostService = require('../../../services/Post.service');
 const PostController = require('../../../controllers/Post.controller');
 
 describe('Testes unitários do controller Post', () => {
-  describe('Testa o comportamento da função insert ao inserir com sucesso um post no banco de dados', () => {
-    const postToInsert = {};
-    const insertedPost = {};
+
+  describe('Testa o comportamento da função findAll ao recuperar todos os posts do banco de dados', () => {
+    const allPosts = [{}];
 
     beforeEach(async () => {
-      sinon.stub(PostService, 'insert').resolves(insertedPost);
+      sinon.stub(PostService, 'findAll').resolves(allPosts);
     });
 
     afterEach(async () => {
-      PostService.insert.restore();
+      PostService.findAll.restore();
     });
 
-    it('Testa se o status de retorno é 201', async () => {
+    it('Testa se o status de retorno é 200', async () => {
       const req = {};
       const res = {};
       req.body = postToInsert;
@@ -24,21 +24,20 @@ describe('Testes unitários do controller Post', () => {
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
 
-      await PostController.insert(req, res);
+      await PostController.findAll(req, res);
 
-      expect(res.status.calledWith(201)).to.be.true;
+      expect(res.status.calledWith(200)).to.be.true;
     });
 
     it('Testa se o json é chamado com o post inserido', async () => {
       const req = {};
       const res = {};
-      req.body = postToInsert;
 
       res.status = sinon.stub().returns(res);
       res.json = sinon.stub().returns();
 
-      PostController.insert(req, res);
+      PostController.findAll(req, res);
 
-      expect(res.json.calledWith(insertedPost)).to.be.true;
+      expect(res.json.calledWith(allPosts)).to.be.true;
     });
   });
