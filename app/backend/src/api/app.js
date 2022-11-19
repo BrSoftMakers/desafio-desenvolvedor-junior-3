d1022a9e2c0bfd7aaeaa683051fda2eadb6e7a44
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-19 17:36:59 -0300

    feat: disponibiliza middleware de erro em app

--- app/backend/src/api/app.js
+++ app/backend/src/api/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const errorMiddleware = require('../middlewares/error.middleware');
 const postRoute = require('../routes/post.route');
 const userRoute = require('../routes/user.route');
 require('express-async-errors');
@@ -14,5 +15,7 @@ app.use(express.json());
 
 app.use('/posts', postRoute);
 app.use('/', userRoute);
+
+app.use(errorMiddleware);
 
 module.exports = app;
