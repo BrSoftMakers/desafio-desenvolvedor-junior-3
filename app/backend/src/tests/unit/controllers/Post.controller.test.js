66de76d8638c8a12e50694204e4545ae23ccc41d
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-18 22:55:38 -0300

    test: implementa testes da função findByUser
    -- Função chama service para buscar todos o posts de um usuário específico no banco de dados;
    -- Se não existirem posts desse usuário, é lançado um erro

--- app/backend/src/tests/unit/controllers/Post.controller.test.js
+++ app/backend/src/tests/unit/controllers/Post.controller.test.js
@@ -5,39 +5,68 @@ const PostController = require('../../../controllers/Post.controller');
 
 describe('Testes unitários do controller Post', () => {
 
-  describe('Testa o comportamento da função findAll ao recuperar todos os posts do banco de dados', () => {
-    const allPosts = [{}];
+  describe('Testa o comportamento da função findByUser', () => {
+    describe('Testa findByUser quando há posts da pessoa usuária no banco de dados', () => {
+      const postsByUser = [{}];
 
-    beforeEach(async () => {
-      sinon.stub(PostService, 'findAll').resolves(allPosts);
+      beforeEach(async () => {
+        sinon.stub(PostService, 'findByUser').resolves(postsByUser);
+      });
+
+      afterEach(async () => {
+        PostService.findByUser.restore();
+      });
+
+      it('Testa se o status de retorno é 200', async () => {
+        const req = {};
+        const res = {};
+        req.body = { userId: 2 };
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        await PostController.findByUser(req, res);
+
+        expect(res.status.calledWith(200)).to.be.true;
+      });
+
+      it('Testa se o json é chamado com todos os posts', async () => {
+        const req = {};
+        const res = {};
+        req.body = { userId: 2 };
+
+        res.status = sinon.stub().returns(res);
+        res.json = sinon.stub().returns();
+
+        await PostController.findByUser(req, res);
+
+        expect(res.json.calledWith(postsByUser)).to.be.true;
+      });
     });
 
-    afterEach(async () => {
-      PostService.findAll.restore();
-    });
+    describe('Testa findByUser quando NÃO há posts da pessoa usuária no banco de dados', () => {
+      beforeEach(async () => {
+        sinon
+          .stub(PostService, 'findByUser')
+          .resolves({ message: 'thereAreNoPostsByThisUser' });
+      });
 
-    it('Testa se o status de retorno é 200', async () => {
-      const req = {};
-      const res = {};
-      req.body = postToInsert;
+      afterEach(async () => {
+        PostService.findByUser.restore();
+      });
 
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns();
-
-      await PostController.findAll(req, res);
-
-      expect(res.status.calledWith(200)).to.be.true;
-    });
-
-    it('Testa se o json é chamado com o post inserido', async () => {
-      const req = {};
-      const res = {};
-
-      res.status = sinon.stub().returns(res);
-      res.json = sinon.stub().returns();
-
-      PostController.findAll(req, res);
-
-      expect(res.json.calledWith(allPosts)).to.be.true;
+      it('Testa se é lançado um erro com a mensagem correta', async () => {
+        const req = {};
+        const res = {};
+        let error;
+        try {
+          req.body = { userId: 25 };
+          await PostController.findByUser(req, res);
+        } catch (err) {
+          error = err;
+        }
+        expect(error.message).to.be.deep.equal('thereAreNoPostsByThisUser');
+      });
     });
   });
+
