0173a70df2146f6175ef65351c7d2fe4dd06ed16
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-20 01:00:37 -0300

    fix: inclui informações de user em retorno das rotas login e register
    --Informações necessárias para uso no frontend

--- app/backend/src/controllers/User.controller.js
+++ app/backend/src/controllers/User.controller.js
@@ -5,13 +5,17 @@ const UserController = {
     const user = await UserService.findUser(req.body);
     if (user.message) throw new Error(user.message);
     const token = UserService.generateToken(user);
-    return res.status(200).json({ token });
+    return res
+      .status(200)
+      .json({ id: user.id, name: user.name, email: user.email, token });
   },
 
   register: async (req, res) => {
     const user = await UserService.insert(req.body);
     const token = UserService.generateToken(user);
-    return res.status(201).json({ token });
+    return res
+      .status(201)
+      .json({ id: user.id, name: user.name, email: user.email, token });
   },
 };
 
