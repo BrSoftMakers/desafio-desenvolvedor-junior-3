f42e593ce24e9d39afae082a2fd0e43235bd08a2
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-20 00:50:42 -0300

    feat: implementa função para buscar user na localStorage

--- app/frontend/src/routes/helpers/storageFunctions.js
+++ app/frontend/src/routes/helpers/storageFunctions.js
@@ -2,3 +2,9 @@ export const saveData = (key, data) => {
   localStorage.setItem(key, JSON.stringify(data));
 };
 
+export const recoverUser = () => {
+  const user = localStorage.getItem('user');
+  if (!user) return null;
+  const parseUser = JSON.parse(user);
+  return parseUser;
+};
