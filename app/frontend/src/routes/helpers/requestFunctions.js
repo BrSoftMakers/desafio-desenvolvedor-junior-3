c3457c787c27e357968980f07d9ccc2d5e54b5c8
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-20 00:51:29 -0300

    feat: implementa requisição de registro de pessoa usuária

--- app/frontend/src/routes/helpers/requestFunctions.js
+++ app/frontend/src/routes/helpers/requestFunctions.js
@@ -3,3 +3,6 @@ import axios from 'axios';
 export const requestLogin = ({ email, password }) =>
   axios.post('http://localhost:3001/login', { email, password });
 
+export const requestRegister = (user) =>
+  axios.post('http://localhost:3001/register', user);
+
