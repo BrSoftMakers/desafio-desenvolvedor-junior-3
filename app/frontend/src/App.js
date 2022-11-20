61b2c65989d21e3039769ea4b028c31964b7eb4f
Airel Ribeiro <mv.ribeirosilva@gmail.com>
2022-11-20 01:02:16 -0300

    feat: inclui rotas e navbar em app

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import NavbarComponent from './components/Navbar';
+import PostForm from './components/PostForm';
+import Login from './routes/Login';
+import Posts from './routes/Posts';
+import Register from './routes/Resgister';
+import SinglePost from './routes/SinglePost';
+import UserPosts from './routes/UserPosts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <NavbarComponent />
+      <h1 className="text-center">
+        <strong>✨ Fantastic Posts ✨</strong>
+      </h1>
+      <h2 className="text-center">
+        as notícias mais reais que você vai encontrar
+      </h2>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/posts" element={<Posts />} />
+        <Route path="/my-posts" element={<UserPosts />} />
+        <Route path="/my-posts/:id" element={<SinglePost />} />
+        <Route path="/create" element={<PostForm />} />
+      </Routes>
     </div>
   );
 }
