d6f736403cd476ef058bf6d018fc17cde67885ed
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-06 15:50:06 -0300

    fix: fix customError with a better message

--- back-end/src/middlewares/Error/customError.ts
+++ back-end/src/middlewares/Error/customError.ts
@@ -1,6 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 
-export class CustomError extends Error {
+class CustomError extends Error {
   public statusCode: number;
 
   constructor(message: string, statusCode: number) {
@@ -9,13 +9,13 @@ export class CustomError extends Error {
   }
 }
 
-export const errorMiddleware = (err: Error, req: Request, res: Response, next: NextFunction) => {
+export const errorMiddleware = (err: Error | CustomError, req: Request, res: Response, next: NextFunction) => {
   try {
     if (err instanceof CustomError) {
       return res.status(err.statusCode).send(err.message);
     } else if (err instanceof Error) {
       return res.status(500).send({
-        helper: 'this error maybe need to be handled by the customError',
+        helper: 'this error was not handled',
         instanceof: err.constructor.name,
         message: err.message,
         stack: err.stack
@@ -25,3 +25,5 @@ export const errorMiddleware = (err: Error, req: Request, res: Response, next: N
     res.status(500).send({ message: 'Internal Server Error, please try again later' });
   }
 };
+
+export default CustomError;
