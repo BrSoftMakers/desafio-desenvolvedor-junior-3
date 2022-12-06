c399d3167ff31dc3db73a57f44242e8438325920
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-06 14:51:33 -0300

    fix: fix typo in login model

--- back-end/src/Login/Login.model.ts
+++ back-end/src/Login/Login.model.ts
@@ -1,7 +1,7 @@
 import prisma from "../lib/PrismaClient";
 
 const handleLogin = async (email: string, password: string) => {
-  const user = await prisma.user.findUnique({
+  const user = await prisma.users.findUnique({
     where: {
       email,
     },
