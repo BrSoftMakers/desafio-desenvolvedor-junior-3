9018ce57d7a6a7ad005030e812643e869c41f966
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-06 13:22:54 -0300

    feat: add login model

---
+++ back-end/src/Login/Login.model.ts
@@ -0,0 +1,22 @@
+import prisma from "../lib/PrismaClient";
+
+const handleLogin = async (email: string, password: string) => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+  // if (!user) {
+  //   return null;
+  // }
+  // const validPassword = await bcrypt.compare(password, user.password);
+  // if (!validPassword) {
+  //   return null;
+  // }
+  // const token = tokenGenerator(user.id);
+  return user;
+}
+
+export default {
+  handleLogin
+};
