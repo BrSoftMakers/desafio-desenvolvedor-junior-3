4c0e253a92564973508ab626c0fbdf1e3d4aaed6
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-06 15:50:50 -0300

    feat: add errorMiddleware to App

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express } from "express";
 import cors from "cors";
 import loginRouter from "./Login/Login.router";
 import RegisterRouter from "./Register/Register.router";
+import { errorMiddleware } from "./middlewares/Error/customError";
 
 const App: Express = express();
 
@@ -9,5 +10,6 @@ App.use(express.json());
 App.use(cors());
 App.use("/login", loginRouter);
 App.use("/register", RegisterRouter);
+App.use(errorMiddleware);
 
 export default App;
