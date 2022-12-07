bd7bfb03f8b56295beee06ffe064c6d6eac25866
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-07 11:58:48 -0300

    feat: add new constantes what will helpe future validations

--- back-end/src/utils/constants.ts
+++ back-end/src/utils/constants.ts
@@ -3,5 +3,15 @@ import path from 'path';
 
 export const SALT_ROUNDS = 10;
 
-// please dont use this secret in production, use a secret that is stored in a .env file or in a secure place
-export const JWT_SECRET = fs.readFileSync(path.join(__dirname, '../../../jwt.key'), 'utf8') || 'secret';
+export const JWT_SECRET = fs.readFileSync(path.join(__dirname, '../../jwt.key'), 'utf8') || 'secret';
+
+export const SECONDS_OF_A_DAY = 24 * 60 * 60;
+
+// should be a character before the @ and a character after the @
+export const EMAIL_REGEX = /^(.+)@(.+)$/;
+
+// name min 2 characters, max 30 characters, only letters and spaces
+export const NAME_REGEX = /^.{4,}$/
+
+// password min 2 characters, max 12, any character 
+export const PASSWORD_REGEX = /^.{4,}$/
