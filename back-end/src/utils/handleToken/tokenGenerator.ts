a4493c2dfdbd906161a75cda8197ce85e72e8488
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-07 12:00:49 -0300

    fix: fix tokengenerator expiresIn to 24 hours

--- back-end/src/utils/handleToken/tokenGenerator.ts
+++ back-end/src/utils/handleToken/tokenGenerator.ts
@@ -1,8 +1,8 @@
 import { sign } from 'jsonwebtoken';
-import { JWT_SECRET } from '../constants';
+import { JWT_SECRET, SECONDS_OF_A_DAY } from '../constants';
 
 const tokenGenerator = (dataYourCouldNeed: any) => {
-  const token = sign(dataYourCouldNeed, JWT_SECRET, { expiresIn: '240h' });
+  const token = sign({ payload: dataYourCouldNeed }, JWT_SECRET, { expiresIn: SECONDS_OF_A_DAY });
   return token;
 }
 
