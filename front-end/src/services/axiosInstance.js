f120b5b4fa6f145f58c8fecb2af7fa55d3ae17d7
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-08 18:57:09 -0300

    fix: fix some eslint issues with headers properties

--- front-end/src/services/axiosInstance.js
+++ front-end/src/services/axiosInstance.js
@@ -1,17 +1,17 @@
-import dotenv from 'dotenv';
-import axios from 'axios';
-dotenv.config();
+import dotenv from 'dotenv'
+import axios from 'axios'
+dotenv.config()
 
-const { REACT_APP_API_BASE_URL } = process.env;
+const { REACT_APP_API_BASE_URL } = process.env
 
-const token = localStorage.getItem('token');
+const token = localStorage.getItem('token')
 
 const axiosInstance = axios.create({
   baseURL: REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
-    'Authorization': `Bearer ${token}`,
-  },
-});
+    Authorization: `Bearer ${token}`
+  }
+})
 
-export default axiosInstance;
+export default axiosInstance
