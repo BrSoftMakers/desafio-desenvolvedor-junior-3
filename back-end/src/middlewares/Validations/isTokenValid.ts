b83b4c7121c0f11b29de502885fb7ea5c9448d49
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-08 06:47:21 -0300

    fix: fix isTokenValid function  with correct types

--- back-end/src/middlewares/Validations/isTokenValid.ts
+++ back-end/src/middlewares/Validations/isTokenValid.ts
@@ -1,19 +1,20 @@
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import { JWT_SECRET } from '../../utils/constants';
 
-const isTokenValid = (req: any, res: any, next: any) => {
+const isTokenValid = (req: Request, res: Response, next: NextFunction) => {
   const token = req.headers.authorization;
   if (!token) {
     return res.status(401).json({ message: 'Token not found' });
   }
   try {
     const decoded = jwt.verify(token, JWT_SECRET);
-    req.user = decoded;
+    (<any>req).decoded = decoded;
     next();
   }
   catch (err) {
     return res.status(401).json({ message: 'Expired or invalid token' });
   }
-}
+};
 
 export default isTokenValid;
