f00e1fe60fde273de1fbe403f8582653dcec64c6
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-08 07:06:16 -0300

    fix: fix module interop node error in posts models

--- back-end/src/Posts/Posts.models.ts
+++ back-end/src/Posts/Posts.models.ts
@@ -4,25 +4,25 @@ import { BAD_REQUEST } from "../middlewares/Error/ErrorConstructor";
 
 const getPosts = async (orderByFilter: string) => {
   const orderBy = orderByFilter === 'asc' ? 'asc' : 'desc';
-  const orderedPosts = prisma.Posts.findMany({ orderBy: { createdAt: orderBy } });
+  const orderedPosts = prisma.posts.findMany({ orderBy: { createdAt: orderBy } });
   return orderedPosts;
 }
 
 const getPostById = async (id: string) => {
-  const post = await prisma.Posts.findUnique({ where: { id } });
+  const post = await prisma.posts.findUnique({ where: { id } });
   if(!post) throw new CustomError(`Post with id ${id} not found`, BAD_REQUEST.statusCode);
   return post;
 }
 
 const createPost = async (title: string, content: string, authorId: string) => {
-  const isAuthorValid = await prisma.Users.findUnique({ where: { id: authorId } });
+  const isAuthorValid = await prisma.users.findUnique({ where: { id: authorId } });
   if(!isAuthorValid) throw new CustomError(`Author with id ${authorId} not found`, BAD_REQUEST.statusCode);
-  const post = await prisma.Posts.create({ data: { title, content, authorId } });
+  const post = await prisma.posts.create({ data: { title, content, authorId } });
   return post;
 }
 
 const updatePost = async (id: string, data: any) => {
-  const post = await prisma.Posts.update({ where: { id }, data });
+  const post = await prisma.posts.update({ where: { id }, data });
   if(!post) throw new CustomError(`Post with id ${id} not found, could note update`, BAD_REQUEST.statusCode);
   return post;
 }
