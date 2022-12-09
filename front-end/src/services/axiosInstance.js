475bd464f607fcb924e0d40364adeefcba56a99b
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-09 10:05:32 -0300

    fix: remove unused console.log inside axiosInstance component

--- front-end/src/services/axiosInstance.js
+++ front-end/src/services/axiosInstance.js
@@ -2,9 +2,6 @@ import axios from 'axios'
 
 const token = localStorage.getItem('token')
 
-console.log(process.env.REACT_APP_API_BASE_URL)
-console.log(token)
-
 const axiosInstance = axios.create({
   baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
