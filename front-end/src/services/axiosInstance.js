c3958d14b8daab80f0e4f316fc4a72c792368385
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-09 10:03:55 -0300

    fix: fix axiosInstance with webpack-dotenv config

--- front-end/src/services/axiosInstance.js
+++ front-end/src/services/axiosInstance.js
@@ -1,13 +1,12 @@
-import dotenv from 'dotenv'
 import axios from 'axios'
-dotenv.config()
-
-const { REACT_APP_API_BASE_URL } = process.env
 
 const token = localStorage.getItem('token')
 
+console.log(process.env.REACT_APP_API_BASE_URL)
+console.log(token)
+
 const axiosInstance = axios.create({
-  baseURL: REACT_APP_API_BASE_URL,
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
     Authorization: `Bearer ${token}`
