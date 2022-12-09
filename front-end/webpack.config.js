542600ccad916ae5a71679b3c7ea91db23c8c68f
AndreyNovaes <andreynovaespro@gmail.com>
2022-12-09 10:02:38 -0300

    feat: add webpack-dotenv to webpack.config.js

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,6 +1,7 @@
 import HtmlWebpackPlugin from 'html-webpack-plugin';
 import path from 'path';
 import { fileURLToPath } from 'url';
+import DotenvWebpackPlugin from 'dotenv-webpack';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -37,6 +38,7 @@ export default {
   plugins: [
     new HtmlWebpackPlugin({
       template: path.join(__dirname, 'public', 'index.html')
-    })
+    }),
+    new DotenvWebpackPlugin()
   ]
 };
