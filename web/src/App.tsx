0d941be8ddbad57b64e8294c9a4dfecc1974d481
Franklin Ramos <franklinramos@outlook.com>
2023-01-04 15:00:23 -0300

    feat: initial web settings

---
+++ web/src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
+export default function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
+}
