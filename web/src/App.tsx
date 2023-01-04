78ce8c131e0068bd23987adcad08f4b9d3c7ebc9
Franklin Ramos <franklinramos@outlook.com>
2023-01-04 20:42:23 -0300

    feat: create blog page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Blog from "./pages/Blog";
 import Home from "./pages/Home";
 
 export default function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/blog" element={<Blog />} />
     </Routes>
   );
 }
