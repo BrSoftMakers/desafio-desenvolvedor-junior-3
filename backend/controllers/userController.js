a42f3f5b65b21fa96b492b5b262a763b6a2f694c
Your Name <dfsilvat@gmail.com>
2023-05-29 21:37:49 -0300

    fix: component position

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -36,11 +36,29 @@ async function registerUser(req, res) {
 
 
  function login(req, res) {
-  const email = req.body.email
-  const password =  req.body.password
 
-  console.log(email, password)
-  res.status(200).send('logado com sucesso')
+  var email = req.body.email
+  var password = req.body.password
+
+  User.findOne({
+    wheres: {
+      email: email
+    }
+  }).then(user => {
+    if(user != undefined){
+      console.log(email)
+      console.log(password)
+      var correct = bcrypt.compareSync(password, user.password)
+      
+      if(correct == true){
+        res.status(200).send('User loged')
+      }else{
+        res.status(404).send('Error')
+      }
+    }else{
+      res.status(404).send('Error')
+    }
+  })
 }
 
 
---
+++ frontend/app/src/components/login form/LoginForm.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react"
+import blogFetch from "../../axios/config";
+
+
+const LoginForm = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+  
+    const handleLogin = async (e) => {
+      e.preventDefault()
+      try{
+            await blogFetch.post('/login', {
+              email,
+              password
+          }).then(() => {
+            alert('logado')
+          })
+          console.log('sucesso')
+      }catch (err) {
+        console.log(err)
+      }
+
+      Array.from(document.querySelectorAll("input")).forEach(
+        input => (input.value = ""))
+    };
+
+
+
+    return(
+        <form action="http://localhost:8000/login" method="post">
+          <h1>Login</h1>
+
+          <input
+            type="email"
+            placeholder="Email"
+            id="email"
+            required
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            id="password"
+            required
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <button type="submit" onClick={(e) => handleLogin(e)}>
+            Sign in
+          </button>
+          {/*  <p>{error}</p> */}
+        </form>
+    )
+}
+
+export default LoginForm;
