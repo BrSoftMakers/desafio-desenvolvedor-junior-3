bbce405b00c928ea0a82c24d17a48f615c51ff3c
João Hélder <joaoheldermartins@hotmail.com>
2023-06-07 13:54:33 -0300

    feat: cria as pastas do backend

---
+++ app/backend/src/api/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const userRouter = require('./routes/userRouter');
+const loginRouter = require('./routes/loginRouter');
+const categoriesRouter = require('./routes/categoriesRouter');
+const postRouter = require('./routes/postRouter');
+
+const app = express();
+
+app.use(express.json());
+app.use('/register', userRouter);
+app.use('/login', loginRouter);
+app.use('/posts', postRouter);
+
+module.exports = app;
---
+++ app/backend/src/api/server.js
@@ -0,0 +1,5 @@
+const port = 3001;
+const app = require('./app');
+
+app.listen(port);
+console.log(`Api rodando na porta ${port}`);
