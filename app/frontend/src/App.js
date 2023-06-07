2b0da0f91d077d813ad81c20a197708e2b78c7bb
João Hélder <joaoheldermartins@hotmail.com>
2023-06-07 16:57:20 -0300

    feat: faz a configuração inicial do app e index do frontend

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Posts from './pages/Posts';
+import PostsDetails from './pages/PostsDetails';
+import UserPosts from './pages/UserPosts';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Switch>
+      <Route exact path="/">
+        < Redirect to='/login' />
+      </Route>
+      <Route path="/login" component={ Login } />
+      <Route path="/register" component={ Register } />
+      <Route path="/posts" Component={ Posts } />
+      <Route exact path="/user/posts" Component={ UserPosts } />
+      <Route path="/user/posts/:id" Component={ PostsDetails } />
+    </Switch>
   );
 }
 
--- app/frontend/src/index.js
+++ app/frontend/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import reportWebVitals from "./reportWebVitals";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import Provider from './context/Provider';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <BrowserRouter>
+      <Provider>
+        <App />
+      </Provider>
+    </BrowserRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
