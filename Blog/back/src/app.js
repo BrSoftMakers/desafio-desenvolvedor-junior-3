2c247ae7684f2c07c56a14860eb176053327636f
tiagoabranges <tiagoabranges@gmail.com>
2023-06-15 15:24:07 -0300

    feat: start server

---
+++ Blog/back/src/app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+module.exports = app;
