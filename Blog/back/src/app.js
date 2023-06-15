89216132678991775047c34b8fa97ac9322c1451
tiagoabranges <tiagoabranges@gmail.com>
2023-06-15 15:56:07 -0300

    feat: ensure sequelize from project

--- Blog/back/src/app.js
+++ Blog/back/src/app.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const router = require('./routes');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(router);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 module.exports = app;
---
+++ Blog/back/src/config/config.js
@@ -0,0 +1,15 @@
+// src/config/config.js
+
+const config = {
+  username: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DATABASE,
+  host: process.env.MYSQL_HOST,
+  dialect: 'mysql',
+};
+
+module.exports = {
+  development: config,
+  test: config,
+  production: config,
+};
