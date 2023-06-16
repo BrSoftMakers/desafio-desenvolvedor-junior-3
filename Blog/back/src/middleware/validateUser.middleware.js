fe066d98d76198efffd64266c204675e56ef2d38
tiagoabranges <tiagoabranges@gmail.com>
2023-06-16 18:54:34 -0300

    feat: Implemented authentication middleware

--- Blog/back/src/middleware/validateUser.middleware.js
+++ Blog/back/src/middleware/validateUser.middleware.js
@@ -1,42 +1,50 @@
-const { User } = require('../models')
-const httpStatus = require('http-status')
+const httpStatus = require('http-status');
+const { User } = require('../models');
 
 const validateEmail = (email) => {
-  const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i
+    const regexEmail = /^[a-z0-9.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/i;
 
-  if (!email) return { status: httpStatus.BAD_REQUEST, message: '"Email" is required' }
-  if (!regexEmail.test(email)) return { status: httpStatus.BAD_REQUEST, message: '"Email" must be a valid email' }
+    if (!email) {
+        return { status: httpStatus.BAD_REQUEST, message: '"Email" is required' };
+    }
+    if (!regexEmail.test(email)) {
+        return { status: httpStatus.BAD_REQUEST, message: '"Email" must be a valid email' };
+    }
 
-  return { status: httpStatus.OK, message: 'OK' }
-}
+    return { status: httpStatus.OK, message: 'OK' };
+};
 
 const validatePassword = (password) => {
-  if (password.length < 6) return { status: httpStatus.BAD_REQUEST, message: '"Password" length must be 6 characters long' }
+    if (password.length < 6) {
+        return {
+            status: httpStatus.BAD_REQUEST,
+            message: '"Password" length must be 6 characters long',
+        };
+    }
 
-  return { status: httpStatus.OK, message: 'OK' }
-}
+    return { status: httpStatus.OK, message: 'OK' };
+};
 
-const validateIfUserExists = async (email) => {
-  const alereadyExists = await User.findOne({ where: { email } })
-
-  if (alereadyExists) return { status: httpStatus.CONFLICT, message: 'User already registered' }
-
-  return { status: httpStatus.OK, message: 'OK' }
-}
+const checkEmailExists = async (email) => {
+    const user = await User.findOne({ where: { email } });
+    return !!user; // Retorna true se o usuário existir, ou false caso contrário
+};
 
 const validateUser = (userData) => {
-  const { email, password } = userData
+    const { email, password } = userData;
 
-  if (validateEmail(email).status !== httpStatus.OK) return validateEmail(email)
-  if (validatePassword(password).status !== httpStatus.OK) return validatePassword(password)
-  if (validateIfUserExists(email).status !== httpStatus.OK) return validateIfUserExists(email)
-
-  return { status: httpStatus.OK, message: 'OK' }
-}
+    if (validateEmail(email).status !== httpStatus.OK) {
+        return validateEmail(email);
+    }
+    if (validatePassword(password).status !== httpStatus.OK) {
+        return validatePassword(password);
+    }
+    return { status: httpStatus.OK, message: 'OK' };
+};
 
 module.exports = {
-  validateEmail,
-  validatePassword,
-  validateIfUserExists,
-  validateUser
+    validateEmail,
+    validatePassword,
+    validateUser,
+    checkEmailExists,
 };
