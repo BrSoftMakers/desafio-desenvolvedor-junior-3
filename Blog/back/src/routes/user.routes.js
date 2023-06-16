f2164bf8ff01d7653fd3997ad1b2157a9833357c
tiagoabranges <tiagoabranges@gmail.com>
2023-06-16 18:53:25 -0300

    delete: Removed user and post routes

--- Blog/back/src/routes/user.routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { createUser } = require('../controllers/user.controllers');
-
-const router = express.Router();
-
-router.post('/register', createUser);
-router.post('/login', );
-
-module.exports = router;
