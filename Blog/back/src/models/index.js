f65d5a3ef97c2cdc202552a5d75c94c95654ac2c
tiagoabranges <tiagoabranges@gmail.com>
2023-06-21 10:17:40 -0300

    readme

--- Blog/back/src/models/index.js
+++ Blog/back/src/models/index.js
@@ -5,7 +5,7 @@ const Sequelize = require('sequelize');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
 // eslint-disable-next-line import/no-dynamic-require
-const config = require(`${__dirname}/../config/config.js`)[env];
+const config = require(`${__dirname}/../config/config.js`)[env]; // configuração nova
 const db = {};
 
 let sequelize;
