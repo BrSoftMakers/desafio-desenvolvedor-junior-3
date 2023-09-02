bab2c45ef1f2490aa1cd9cb382291fe0b0f97282
MylennaAlcantara <mylenna.a.a.corinthiana@gmail.com>
2023-09-02 00:13:18 -0300

    Criado as rotas

--- Frontend/desafio-frontend/src/App.js
+++ Frontend/desafio-frontend/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router';
+import { Login } from './pages/login';
+import { Posts } from './pages/posts';
+import { CriarPost } from './pages/criar';
+import { EditarPost } from './pages/editar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="" Component={Login} />
+        <Route path="/posts" Component={Posts} />
+        <Route path="/post/novo" Component={CriarPost} />
+        <Route path="/post/{id}" Component={EditarPost} />
+      </Routes>
     </div>
   );
 }
