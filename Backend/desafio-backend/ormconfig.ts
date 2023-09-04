66d23d6419f0c4e3846fc56a8305a773f5f32bd5
MylennaAlcantara <mylenna.a.a.corinthiana@gmail.com>
2023-09-03 23:26:31 -0300

    configuração para deploy

--- Backend/desafio-backend/ormconfig.ts
+++ Backend/desafio-backend/ormconfig.ts
@@ -3,12 +3,12 @@ import { DataSourceOptions } from "typeorm";
 // Arquivo contendo as configurações de acesso ao banco de dados
 
 export const ormConfig: DataSourceOptions = {
-    "database": "desafio_junior_3", // Nome do banco de dados
-    "host": "localhost", // Nome do host que esta alocado o banco de dados
+    "database": "blpvdpisjtnfbb6c13jk", // Nome do banco de dados
+    "host": "blpvdpisjtnfbb6c13jk-mysql.services.clever-cloud.com", // Nome do host que esta alocado o banco de dados
     "type": "mysql", // Tipo do banco de dados 
     "port": 3306, // Porta que foi configurada para acesso ao banco de dados
-    "username": "root", // Usuario de acesso ao banco de dados insira o seu usuario de acesso
-    "password": "root",// Senha de acesso ao banco de dados insira a sua senha de acesso
+    "username": "uzozut4jzpeqdwkk", // Usuario de acesso ao banco de dados insira o seu usuario de acesso
+    "password": "4t5mhh2G05ssbNkCm08f",// Senha de acesso ao banco de dados insira a sua senha de acesso
     "synchronize": true,
     "entities": ["dist/**/*.model.js"]
 }
